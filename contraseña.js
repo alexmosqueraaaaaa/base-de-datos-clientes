573d32e1fc410d7620a1fa7ea9c3e66f3f74d84f
a22alejandromc <a22alejandromc@iessanclemente.net>
2022-10-21 12:06:48 +0200

    js

--- contraseña.js
+++ contraseña.js
@@ -1,11 +1,12 @@
 function ir(){
-    var c=1234;
-    var u="flat avenida de lugo";
-    if(document.form.password.value==c && document.form.login.value==u){
-        alert("Welcome to our website");
-        window.location="formulario.html";
-    }
+  var c=1234;
+  var u="flatavenidadelugo@gmail.com";
+  if(document.form.password.value==c && document.form.login.value==u){
+    alert("Welcome to our Website");
+    window.location="formulario.html";}
     else{
-        alert("please enter correct username and password");
+        alert("Please enter a valid email and password");
     }
-    }
+  }
+  
+
