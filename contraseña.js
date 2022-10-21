ca4e88b3953b9e00ba27ca2ec72b942914a87454
a22alejandromc <a22alejandromc@iessanclemente.net>
2022-10-21 11:56:57 +0200

    clientes

---
+++ contraseña.js
@@ -0,0 +1,11 @@
+function ir(){
+    var c=1234;
+    var u="flat avenida de lugo";
+    if(document.form.password.value==c && document.form.login.value==u){
+        alert("Welcome to our website");
+        window.location="formulario.html";
+    }
+    else{
+        alert("please enter correct username and password");
+    }
+    }
