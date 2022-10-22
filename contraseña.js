fecb9a26d644a4e5ee95c0df59c4f188ee3d2bfc
a22alejandromc <a22alejandromc@iessanclemente.net>
2022-10-22 17:08:28 +0200

    final

--- contraseña.js
+++ contraseña.js
@@ -1,9 +1,9 @@
 function ir(){
   var c=1234;
-  var u="flatavenidadelugo@gmail.com";
+  var u="apartamentociudaddelacultura@gmail.com";
   if(document.form.password.value==c && document.form.login.value==u){
     alert("Welcome to our Website");
-    window.location="formulario.html";}
+    window.location="file:///C:/Users/Alex/Downloads/formulario%20clientes/index.html";}
     else{
         alert("Please enter a valid email and password");
     }
